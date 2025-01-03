dd0b385c1bb958e1da522f25f1ab75527ef41915
ADF <kevin.alarcon9@t-mobile.com>
2025-01-02 21:29:12 -0500

    Initial Commit

---
+++ no-sweat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  no-sweat
+//
+//  Created by Alarcon, Kevin on 1/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ no-sweat/no_sweatApp.swift
@@ -0,0 +1,17 @@
+//
+//  no_sweatApp.swift
+//  no-sweat
+//
+//  Created by Alarcon, Kevin on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct no_sweatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
